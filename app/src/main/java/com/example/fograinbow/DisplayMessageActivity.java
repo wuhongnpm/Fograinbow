0a34da8a12fb22d4728d67e301890b819511c55e
SoapEye <lious.q.k@gmail.com>
2020-06-30 08:39:06 +0800

    2

---
+++ app/src/main/java/com/example/fograinbow/DisplayMessageActivity.java
@@ -0,0 +1,24 @@
+package com.example.fograinbow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        //001
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //002
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
--- app/src/main/java/com/example/fograinbow/MainActivity.java
+++ app/src/main/java/com/example/fograinbow/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.fograinbow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.view.Display;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.fograinbow.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText  editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
+
 }
